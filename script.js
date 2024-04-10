57e7589bb5422f8f68840daa96a7c830c05a2c52
mj-weshh <jowamumjwesh@gmail.com>
2024-04-10 20:21:12 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const display1 = document.querySelector('#projectsDisplay')
+function projectdisplay(){
+     for(let i = 1; i<7; i++){
+         display1.innerHTML(
+          <div>
+               <img src='./images/img'/>
+          </div>
+         )
+     }
+}
+
+const projects = {
+          'id': 1,
+          'img': <img src="./images/..."></img>,
+          'description': <p></p>
+}
